bf439306c64b55f17cbaf807f088bf2499dbdbf3
saresend <saresend@usc.edu>
2020-05-11 21:03:24 -0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import GoogleMapReact from 'google-map-react';
 
 function App() {
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <TitleBar>
+        <Title>Where is Menlo?? :(</Title>
+      </TitleBar>
+      <div style={{ height: '90vh', width: '100%' }}>
+      <GoogleMapReact
+          bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+          defaultCenter={11}
+          defaultZoom={{
+            lat: 59.95,
+            lng: 30.33
+          }}
         >
-          Learn React
-        </a>
-      </header>
+        </GoogleMapReact>
+        </div>
     </div>
   );
 }
+
+const TitleBar = styled.div`
+  background: white;
+  display: flex;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  font-size: 30px;
+`;
 
 export default App;
